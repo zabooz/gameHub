d50ab049fa19008f73680fad46e8ab05aedb52b7
zaboo <zabooz1988@gmail.com>
2024-06-27 22:19:20 +0200

    changed number of collums on large screens

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -24,7 +24,7 @@ function GameGrid({genre}:Props) {
     <>
       {error && <Text>error</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         spacing={3}
         padding={5}
       >
